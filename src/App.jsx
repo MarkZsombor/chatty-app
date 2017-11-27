69bc78c8fde76ede0f0ab04ae5083cb15ceae57e
MarkZsombor <markszsombor@gmail.com>
2017-11-27 23:26:15 +0000

    wrote initial jsx files, page compiles properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
