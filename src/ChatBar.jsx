c6cb78bcc7150cc82dc21ba2a6075830583cbb04
MarkZsombor <markszsombor@gmail.com>
2017-11-29 21:35:16 +0000

    username now changes on blur as well as on enter

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -11,10 +11,12 @@ class ChatBar extends Component {
       }
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.changeUserName = this.changeUserName.bind(this);
+    this.changeUserNameEnter = this.changeUserNameEnter.bind(this);
+    this.changeUserNameBlur = this.changeUserNameBlur.bind(this);
   }
 
-  changeUserName(event) {
+  //Changes username when enter is pressed while focus is on name input
+  changeUserNameEnter(event) {
     if(event.key == 'Enter') {
       const state = {
         error: ''
@@ -22,6 +24,15 @@ class ChatBar extends Component {
       this.props.onUserNameChange(event.target.value);
     }
   };
+
+  //Changes username when focus is lost from name input
+  changeUserNameBlur(event) {
+    const state = {
+      error: ''
+    };
+    this.props.onUserNameChange(event.target.value);
+  };
+
 
   handleKeyPress(event) {
     // Works for object database
@@ -41,7 +52,8 @@ class ChatBar extends Component {
         <input className="chatbar-username"
           defaultValue={ this.props.userName.name }
           placeholder="Your Name (Optional)"
-          onKeyPress={ this.changeUserName } />
+          onKeyPress={ this.changeUserNameEnter }
+          onBlur={this.changeUserNameBlur} />
         <input className="chatbar-message"
           placeholder="Type a message and hit ENTER"
           onKeyPress={ this.handleKeyPress } />
